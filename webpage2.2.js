384ef0f6290586413ceb059de21a296a8c248c03
Fantaste-Vilte <117042941+Fantaste-Vilte@users.noreply.github.com>
2022-12-20 22:04:32 +0100

    Add files via upload

---
+++ webpage2.2.js
@@ -0,0 +1,16 @@
+
+function logChoice(log) {
+    return log
+}
+
+function userChoice1(choice) {
+    let options1 = []
+        if (choice === 1) {
+            options1.push(1)
+        }  else if (choice === 2) {
+            options1.push(2)
+        }   else if (choice === 3) {
+            options2.push(3)
+        } else {}
+    return options1
+}
